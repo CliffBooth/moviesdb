f0a874f08936864c49cc8e62647fb4c05e29bf76
CliffBooth <feraljerboa@gmail.com>
2022-04-21 21:16:37 +0300

    save pre-existed data

--- lab2/src/main/kotlin/main.kt
+++ lab2/src/main/kotlin/main.kt
@@ -1,3 +1,5 @@
+import org.postgresql.util.PSQLException
+import java.sql.Connection
 import java.sql.DriverManager
 import java.sql.Statement
 import java.time.LocalDate
@@ -12,235 +14,200 @@ const val firstNameFile = "names.txt"
 const val secondNameFile = "secondNames.txt"
 const val titlesFile = "movie_titles.txt"
 
-const val entriesNumber = 5000
-
 val firstNames = HashSet<String>().apply {
-    User::class.java.getResourceAsStream(firstNameFile)!!.bufferedReader().useLines { addAll(it) }
+    Adder::class.java.getResourceAsStream(firstNameFile)!!.bufferedReader().useLines { addAll(it) }
 }
 
 val secondNames = HashSet<String>().apply {
-    User::class.java.getResourceAsStream(secondNameFile)!!.bufferedReader().useLines { addAll(it) }
+    Adder::class.java.getResourceAsStream(secondNameFile)!!.bufferedReader().useLines { addAll(it) }
 }
 
+val titles = HashSet<String>().apply {
+    Adder::class.java.getResourceAsStream(titlesFile)!!.bufferedReader().useLines { addAll(it) }
+}
+
+const val entriesNumber = 10_000
+
+val c: Connection = DriverManager.getConnection(jdbcUrl, userName, password)
+val statement: Statement = c.createStatement()
+
 fun main() {
-    val c = DriverManager.getConnection(jdbcUrl, userName, password)
-    val statement = c.createStatement()
-
-    clearTable(statement)
-
-    getUsers(entriesNumber).forEach {
-        statement.execute("INSERT INTO users(email, username) VALUES('${it.email}','${it.userName}')")
-    }
+    Adder.addUsers(entriesNumber)
     println("users")
 
-    getPersonalities(entriesNumber).forEach {
-        val q = """
-            INSERT INTO personalities(name, date_of_birth, age)
-            VALUES('${it.name}','${it.dateOfBirth}','${it.age}')
-        """.trimIndent()
-        statement.execute(q)
-    }
+    Adder.addPersonalities(entriesNumber)
     println("personalities")
 
-    getMovies(entriesNumber).forEach {
-        statement.execute("INSERT INTO movies(title, date_released) VALUES('${it.title}','${it.dateReleased}')")
-    }
+    Adder.getMovies(entriesNumber)
     println("movies")
 
-    getPersonalityToMovie(statement, entriesNumber).forEach {
-        statement.execute("INSERT INTO screenwriter_to_movie(personality_id, movie_id) " +
-                "VALUES('${it.personalityId}','${it.movieId}')")
-    }
+    Adder.addPersonalityToMovie(entriesNumber, "director_to_movie")
+    println("director")
+    Adder.addPersonalityToMovie(entriesNumber, "actor_to_movie")
+    println("actor")
+    Adder.addPersonalityToMovie(entriesNumber, "screenwriter_to_movie")
     println("screenwriter")
 
-    getPersonalityToMovie(statement, entriesNumber).forEach {
-        statement.execute("INSERT INTO director_to_movie(personality_id, movie_id) " +
-                "VALUES('${it.personalityId}','${it.movieId}')")
-    }
-    println("director")
-
-    getPersonalityToMovie(statement, entriesNumber).forEach {
-        statement.execute("INSERT INTO actor_to_movie(personality_id, movie_id) " +
-                "VALUES('${it.personalityId}','${it.movieId}')")
-    }
-    println("actor")
-
-    getRating(statement, entriesNumber).forEach {
-        statement.execute("INSERT INTO rating(user_id, movie_id, rating) " +
-                "VALUES('${it.userId}','${it.movieId}','${it.rating}')")
-    }
+    Adder.addRating(entriesNumber)
     println("rating")
 
-    getReviews(statement, entriesNumber).forEach {
-        statement.execute("INSERT INTO review(user_id, movie_id, review) " +
-                "VALUES('${it.userId}','${it.movieId}','${it.review}')")
-    }
+    Adder.addReviews(entriesNumber)
     println("review")
 
-    getMovieLists(statement, entriesNumber).forEach {
-        statement.execute("INSERT INTO movie_list(user_id, list_name) VALUES('${it.userId}','${it.name}')")
-    }
+    Adder.addMovieLists(entriesNumber)
     println("movieList")
 
-    getMovieToLists(statement, entriesNumber).forEach {
-        statement.execute("INSERT INTO movie_to_list(movie_id, list_id) " +
-                "VALUES('${it.movieId}','${it.listId}')")
-    }
+    Adder.addMovieToLists(entriesNumber)
     println("movie to list")
 }
 
-fun getUsers(number: Int) = HashSet<User>().apply {
-    var emailCounter: Long = 1
-    repeat(number) {
-        val email = "email${emailCounter++}@mail.com"
-        add(User(email = email, userName = firstNames.random()))
+object Adder {
+
+    private fun getIds(table: String) = HashSet<Int>().apply {
+        val rs = statement.executeQuery("SELECT id FROM $table")
+        while (rs.next()) {
+            add(rs.getInt("id"))
+        }
+    }
+
+    private fun getUniqueIdPairs(existingPairs: Set<Pair<Int, Int>>, table1: String, table2: String, number: Int) =
+        mutableSetOf<Pair<Int, Int>>().apply {
+            val ids1 = getIds(table1)
+            val ids2 = getIds(table2)
+            repeat(number) {
+                var id1: Int?
+                var id2: Int?
+                do {
+                    id1 = ids1.random()
+                    id2 = ids2.random()
+                } while (contains(Pair(id1!!, id2!!)) || existingPairs.contains(Pair(id1, id2)))
+                add(Pair(id1, id2))
+            }
+        }
+
+    private fun getRandomString(maxLength: Int, whiteSpace: Boolean = true): String {
+        val charPool = "q,w,e,r,t,y,u,i,o,p,a,s,d,f,g,h,j,k,l,z,x,c,v,b,n,m, "
+            .split(",")
+            .dropLastWhile { !whiteSpace && it == " " }
+        val length = (1..maxLength).random()
+        return (0..length)
+            .map { Random.nextInt(0, charPool.size) }.joinToString("", transform = charPool::get)
+    }
+
+    fun addUsers(number: Int) {
+        var i = 0
+        while (i < number) {
+            i++
+            val email = "${getRandomString(30, false)}@mail.com"
+            try {
+                statement.execute("INSERT INTO users(email, username) VALUES('${email}','${firstNames.random()}')")
+            } catch (e: PSQLException) {
+                i--
+                continue
+            }
+        }
+    }
+
+    fun addPersonalities(number: Int) {
+        repeat(number) {
+            val name = "${firstNames.random()} ${secondNames.random()}"
+            val dayOffset = (0..ChronoUnit.DAYS.between(
+                LocalDate.of(1940, 1, 1),
+                LocalDate.of(2004, 1, 1)
+            )).random()
+            val date = LocalDate.of(1940, 1, 1).plusDays(dayOffset)
+            val age = ChronoUnit.YEARS.between(date, LocalDate.now()).toInt()
+            statement.execute("INSERT INTO personalities(name, date_of_birth, age) VALUES('${name}','${date}','${age}')")
+        }
+}
+
+    fun getMovies(number: Int) {
+        repeat(number) {
+            val dayOffset = (0..ChronoUnit.DAYS.between(
+                LocalDate.of(1940, 1, 1),
+                LocalDate.of(2022, 4, 21)
+            )).random()
+            val date = LocalDate.of(1940, 1, 1).plusDays(dayOffset)
+            val title = titles.random().replace("'", "''")
+            statement.execute("INSERT INTO movies(title, date_released) VALUES('${title}','${date}')")
+        }
+}
+
+    fun addPersonalityToMovie(number: Int, table: String) {
+        val existingPairs = mutableSetOf<Pair<Int, Int>>()
+        val rs = statement.executeQuery("SELECT * FROM $table")
+        while (rs.next()) {
+            existingPairs.add(Pair(rs.getInt("personality_id"), rs.getInt("movie_id")))
+        }
+
+        val ids = getUniqueIdPairs(existingPairs,"personalities", "movies", number)
+        for ((personalityId, movieId) in ids)
+            statement.execute("INSERT INTO ${table}(personality_id, movie_id) VALUES('${personalityId}','${movieId}')")
+    }
+
+    fun addRating(number: Int) {
+        val existingPairs = mutableSetOf<Pair<Int, Int>>()
+        val rs = statement.executeQuery("SELECT * FROM rating")
+        while (rs.next()) {
+            existingPairs.add(Pair(rs.getInt("user_id"), rs.getInt("movie_id")))
+        }
+
+        val ids = getUniqueIdPairs(existingPairs, "users", "movies", number)
+        for ((userId, movieId) in ids) {
+            val rating = (1..10).random()
+            statement.execute("INSERT INTO rating(user_id, movie_id, rating) VALUES('$userId','$movieId','$rating')")
+        }
+    }
+
+    fun addReviews(number: Int) {
+        val existingPairs = mutableSetOf<Pair<Int, Int>>()
+        val rs = statement.executeQuery("SELECT * FROM review")
+        while (rs.next()) {
+            existingPairs.add(Pair(rs.getInt("user_id"), rs.getInt("movie_id")))
+        }
+
+        val ids = getUniqueIdPairs(existingPairs, "users", "movies", number)
+        for ((userId, movieId) in ids) {
+            val review = getRandomString(200)
+            statement.execute("INSERT INTO review(user_id, movie_id, review) VALUES('$userId','$movieId','$review')")
+        }
+    }
+
+    fun addMovieLists(number: Int) {
+        val userIds = getIds("users")
+        repeat(number) {
+            val name = getRandomString(15)
+            val userId = userIds.random()
+            statement.execute("INSERT INTO movie_list(user_id, list_name) VALUES('$userId','$name')")
+        }
+    }
+
+    fun addMovieToLists(number: Int) {
+        val existingPairs = mutableSetOf<Pair<Int, Int>>()
+        val rs = statement.executeQuery("SELECT * FROM movie_to_list")
+        while (rs.next()) {
+            existingPairs.add(Pair(rs.getInt("movie_id"), rs.getInt("list_id")))
+        }
+
+        val ids = getUniqueIdPairs(existingPairs,"movies", "movie_list", number)
+        for ((movieId, listId) in ids)
+            statement.execute("INSERT INTO movie_to_list(movie_id, list_id)VALUES('$movieId','$listId')")
+    }
+
+
+    fun clearTable() {
+        val tables = listOf(
+            "personalities",
+            "screenwriter_to_movie",
+            "movies",
+            "actor_to_movie",
+            "director_to_movie",
+            "screenwriter_to_movie",
+            "users",
+            "movie_list",
+            "rating",
+            "review",
+        )
+        tables.forEach { statement.execute("DELETE FROM $it") }
     }
 }
-
-fun getPersonalities(number: Int) = HashSet<Personality>().apply {
-    repeat(number) {
-        val name = "${firstNames.random()} ${secondNames.random()}"
-        val dayOffset = (0..ChronoUnit.DAYS.between(
-            LocalDate.of(1940, 1, 1),
-            LocalDate.of(2004, 1, 1)
-        )).random()
-        val date = LocalDate.of(1940, 1, 1).plusDays(dayOffset)
-        val age = ChronoUnit.YEARS.between(date, LocalDate.now()).toInt()
-        add(Personality(name = name, dateOfBirth = date.toString(), age = age))
-    }
-}
-
-fun getMovies(number: Int) = HashSet<Movie>().apply {
-    val titles = HashSet<String>().apply {
-        User::class.java.getResourceAsStream(titlesFile)!!.bufferedReader().useLines { addAll(it) }
-    }
-    repeat(number) {
-        val dayOffset = (0..ChronoUnit.DAYS.between(
-            LocalDate.of(1940, 1, 1),
-            LocalDate.of(2022, 4, 21)
-        )).random()
-        val date = LocalDate.of(1940, 1, 1).plusDays(dayOffset)
-        val title = titles.random().replace("'", "''")
-        add(Movie(title = title, dateReleased = date.toString()))
-    }
-}
-
-private fun getIds(statement: Statement, table: String) = HashSet<Int>().apply {
-    val rs = statement.executeQuery("SELECT id FROM $table")
-    while (rs.next()) {
-        add(rs.getInt("id"))
-    }
-}
-
-private fun getRandomString(maxLength: Int): String {
-    val charPool = "q,w,e,r,t,y,u,i,o,p,a,s,d,f,g,h,j,k,l,z,x,c,v,b,n,m, ".split(",")
-    val length = (1..maxLength).random()
-    return (0..length)
-        .map { Random.nextInt(0, charPool.size) }.joinToString("", transform = charPool::get)
-}
-
-
-fun getPersonalityToMovie(statement: Statement, number: Int): Set<PersonalityToMovie> {
-    val personalityIds = getIds(statement, "personalities")
-    val movieIds = getIds(statement, "movies")
-    val inserted = mutableSetOf<PersonalityToMovie>()
-    repeat(number) {
-        var ptm: PersonalityToMovie?
-        do {
-            ptm = PersonalityToMovie(personalityIds.random(), movieIds.random())
-        } while (inserted.contains(ptm))
-        inserted.add(ptm!!)
-    }
-    return inserted
-}
-
-fun getRating(statement: Statement, number: Int): Set<Rating> {
-    val movieIds = getIds(statement, "movies")
-    val userIds = getIds(statement, "users")
-    val inserted = mutableSetOf<Pair<Int, Int>>()
-    val result = mutableSetOf<Rating>()
-    repeat(number) {
-        var userId: Int?
-        var movieId: Int?
-        do {
-            userId = userIds.random()
-            movieId = movieIds.random()
-        } while (inserted.contains(Pair(userId!!, movieId!!)))
-        inserted.add(Pair(userId, movieId))
-        result.add(Rating(userId, movieId, (1..10).random()))
-    }
-    return result
-}
-
-fun getReviews(statement: Statement, number: Int): Set<Review> {
-    val movieIds = getIds(statement, "movies")
-    val userIds = getIds(statement, "users")
-    val result = mutableSetOf<Review>()
-    val inserted = mutableSetOf<Pair<Int, Int>>()
-    repeat(number) {
-        var userId: Int?
-        var movieId: Int?
-        do {
-            userId = userIds.random()
-            movieId = movieIds.random()
-        } while (inserted.contains(Pair(userId!!, movieId!!)))
-        inserted.add(Pair(userId, movieId))
-        val review = getRandomString(200)
-        result.add(Review(userId, movieId, review))
-    }
-    return result
-}
-
-fun getMovieLists(statement: Statement, number: Int) = HashSet<MovieList>().apply {
-    val userIds = getIds(statement, "users")
-    repeat(number) {
-        add(MovieList(userIds.random(), getRandomString(15)))
-    }
-}
-
-fun getMovieToLists(statement: Statement, number: Int): Set<MovieToList> {
-    val movieIds = getIds(statement, "movies")
-    val listIds = getIds(statement, "movie_list")
-    val inserted = mutableSetOf<MovieToList>()
-    repeat(number) {
-        var mtl: MovieToList?
-        do {
-            mtl = MovieToList(movieIds.random(), listIds.random())
-        } while (inserted.contains(mtl))
-        inserted.add(mtl!!)
-    }
-    return inserted
-}
-
-
-fun clearTable(statement: Statement) {
-    val tables = listOf(
-        "personalities",
-        "screenwriter_to_movie",
-        "movies",
-        "actor_to_movie",
-        "director_to_movie",
-        "screenwriter_to_movie",
-        "users",
-        "movie_list",
-        "rating",
-        "review",
-    )
-    tables.forEach { statement.execute("DELETE FROM $it") }
-}
-
-data class Personality(val name: String, val dateOfBirth: String, val age: Int)
-
-data class Movie(val title: String, val dateReleased: String)
-
-data class User(val email: String, val userName: String)
-
-data class PersonalityToMovie(val personalityId: Int, val movieId: Int)
-
-data class Rating(val userId: Int, val movieId: Int, val rating: Int)
-
-data class Review(val userId: Int, val movieId: Int, val review: String)
-
-data class MovieList(val userId: Int, val name: String)
-
-data class MovieToList(val movieId: Int, val listId: Int)
